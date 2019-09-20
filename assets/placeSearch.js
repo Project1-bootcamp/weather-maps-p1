14af5316559798e4850adefa9d954265cb14c84e
joe8021 <vanhornjoe8@gmail.com>
2019-09-20 13:53:51 -0500

    map created, along with search bar that allows use to search a location. Need to add feature to display nearby places

---
+++ assets/placeSearch.js
@@ -0,0 +1,57 @@
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+
+var map;
+var service;
+var infowindow;
+
+var userSearch = "Chicago,IL";
+
+console.log($("#location").val())
+
+$("#submit").on("click", function(){
+    userSearch = $("#location").val();
+    initMap();
+})
+
+function initMap() {
+    var sydney = new google.maps.LatLng(-33.867, 151.195);
+
+    infowindow = new google.maps.InfoWindow();
+
+    map = new google.maps.Map(
+        document.getElementById('map'), { center: sydney, zoom: 11 });
+
+    var request = {
+        query: userSearch,
+        fields: ['name', 'geometry'],
+    };
+
+    console.log(userSearch);
+
+    service = new google.maps.places.PlacesService(map);
+
+    service.findPlaceFromQuery(request, function (results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+                createMarker(results[i]);
+            }
+
+            map.setCenter(results[0].geometry.location);
+        }
+    });
+}
+
+function createMarker(place) {
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function () {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+    });
+}
